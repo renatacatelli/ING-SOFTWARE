244918218411a8c29035129b2bdb428648425952
renatacatelli <renatacatelli15@gmail.com>
2024-04-11 18:23:39 -0300

    [Parte 2.1] Configuracion de base de datos.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+
 ]
 
 MIDDLEWARE = [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'C:/Users/renat/Act0-IS/mysite/db.sqlite3',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT-3'
 
 USE_I18N = True
 
